5277857a3cb1b1ca438591b8d1aec89ff119992d
Nicolas Boute <4249@holbertonschool.com>
2022-05-10 10:24:19 +0200

    added basic code structures

--- main.h
+++ main.h
@@ -1,6 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef __MAIN_H__
+#define __MAIN_H__
 
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -10,12 +12,12 @@
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
  */
-typedef struct stack_s
+typedef struct		stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
-} stack_t;
+	int				n;
+	struct stack_s	*prev;
+	struct stack_s	*next;
+}					stack_t;
 
 /**
  * struct instruction_s - opcode and its function
@@ -25,10 +27,32 @@ typedef struct stack_s
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO
  */
-typedef struct instruction_s
+typedef struct		instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
+	char		*opcode;
+	void		(*f)(stack_t **stack, unsigned int line_number);
+}					instruction_t;
 
-#endif /* MAIN_H */
+/**
+ * @brief
+ *
+ */
+typedef struct		instruction_list_s
+{
+	char						*str;
+	struct instruction_list_s	*next;
+}					instruction_list_t;
+
+/**
+ * @brief
+ *
+ */
+typedef struct		data_s
+{
+	stack_t				*stack;
+	instruction_t		*functions;
+	instruction_list_t	*instructions;
+}					data_t;
+
+
+#endif /* __MAIN_H__ */
