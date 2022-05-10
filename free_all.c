6e9d1535328fe9cb6a30d3478b6b6d20261a8f97
Nicolas Boute <4249@holbertonschool.com>
2022-05-10 14:40:24 +0200

    fixed merge errors

---
+++ free_all.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+void	free_functions(instruction_t *functions)
+{
+	int	i;
+
+	for (i = 0; i < NB_FUNCTIONS; i++)
+		_memdel((void **)&functions->opcode);
+}
+
+void	free_instructions(instruction_list_t **list)
+{
+	instruction_list_t	*ptr;
+
+	while (*list)
+	{
+		ptr = *list;
+		*list = (*list)->next;
+		printf("FREEING ARG\n");
+		_memdel((void **)&ptr->argument);
+		free(ptr);
+	}
+}
+
+void	free_stack(stack_t **stack)
+{
+	stack_t	*ptr;
+
+	while (*stack)
+	{
+		ptr = *stack;
+		*stack = (*stack)->next;
+		free(ptr);
+	}
+}
+
+void	free_all(data_t	*data)
+{
+	free_functions(data->functions);
+	free_instructions(&data->instructions);
+	free_stack(&data->stack);
+}
--- main.c
+++ main.c
@@ -10,14 +10,13 @@ int main(int ac, char **av)
 {
 	data_t data;
 
-	initialize_data(&data);
 	if (ac != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	if(initialize_data(&data) != 0)
-		return (EXIT_FAILURE)
+		return (EXIT_FAILURE);
 	if (parse_file(&data, av[1]) != 0)
 	{
 		free_all(&data);
