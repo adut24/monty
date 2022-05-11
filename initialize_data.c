dbc5947a419d2daad7a8bb8fc9391d0f5e180613
Nicolas Boute <4249@holbertonschool.com>
2022-05-11 10:34:23 +0200

    fixed betty on initialize_data.c

--- initialize_data.c
+++ initialize_data.c
@@ -1,8 +1,80 @@
 #include "main.h"
 
 /**
+ * initialize_data3 - initialize the data structure
+ * @i: Array index
+ * Return: EXIT_FAILURE or 0 in case of success
+ */
+int	initialize_data3(int i)
+{
+	data.functions[i].opcode = strdup("rotl");
+	if (!data.functions[i].opcode)
+		return (EXIT_FAILURE);
+	data.functions[i++].f = rotl;
+
+	data.functions[i].opcode = strdup("rotr");
+	if (!data.functions[i].opcode)
+		return (EXIT_FAILURE);
+	data.functions[i++].f = rotr;
+
+	data.functions[i].opcode = strdup("stack");
+	if (!data.functions[i].opcode)
+		return (EXIT_FAILURE);
+	data.functions[i++].f = change_into_stack;
+
+	data.functions[i].opcode = strdup("queue");
+	if (!data.functions[i].opcode)
+		return (EXIT_FAILURE);
+	data.functions[i++].f = change_into_queue;
+	return (0);
+}
+
+/**
+ * initialize_data2 - initialize the data structure
+ * @i: Array index
+ * Return: EXIT_FAILURE or 0 in case of success
+ */
+int	initialize_data2(int i)
+{
+	data.functions[i].opcode = strdup("add");
+	if (!data.functions[i].opcode)
+		return (EXIT_FAILURE);
+	data.functions[i++].f = op_add;
+
+	data.functions[i].opcode = strdup("sub");
+	if (!data.functions[i].opcode)
+		return (EXIT_FAILURE);
+	data.functions[i++].f = op_sub;
+
+	data.functions[i].opcode = strdup("div");
+	if (!data.functions[i].opcode)
+		return (EXIT_FAILURE);
+	data.functions[i++].f = op_div;
+
+	data.functions[i].opcode = strdup("mul");
+	if (!data.functions[i].opcode)
+		return (EXIT_FAILURE);
+	data.functions[i++].f = op_mul;
+
+	data.functions[i].opcode = strdup("mod");
+	if (!data.functions[i].opcode)
+		return (EXIT_FAILURE);
+	data.functions[i++].f = op_mod;
+
+	data.functions[i].opcode = strdup("pchar");
+	if (!data.functions[i].opcode)
+		return (EXIT_FAILURE);
+	data.functions[i++].f = pchar;
+
+	data.functions[i].opcode = strdup("pstr");
+	if (!data.functions[i].opcode)
+		return (EXIT_FAILURE);
+	data.functions[i++].f = pstr;
+	return (initialize_data3(i));
+}
+
+/**
  * initialize_data - initialize the data structure
- * @data: pointer to the data_t struct
  * Return: EXIT_FAILURE or 0 in case of success
  */
 int	initialize_data(void)
@@ -42,60 +114,5 @@ int	initialize_data(void)
 	if (!data.functions[i].opcode)
 		return (EXIT_FAILURE);
 	data.functions[i++].f = NULL;
-
-	data.functions[i].opcode = strdup("add");
-	if (!data.functions[i].opcode)
-		return (EXIT_FAILURE);
-	data.functions[i++].f = op_add;
-
-	data.functions[i].opcode = strdup("sub");
-	if (!data.functions[i].opcode)
-		return (EXIT_FAILURE);
-	data.functions[i++].f = op_sub;
-
-	data.functions[i].opcode = strdup("div");
-	if (!data.functions[i].opcode)
-		return (EXIT_FAILURE);
-	data.functions[i++].f = op_div;
-
-	data.functions[i].opcode = strdup("mul");
-	if (!data.functions[i].opcode)
-		return (EXIT_FAILURE);
-	data.functions[i++].f = op_mul;
-
-	data.functions[i].opcode = strdup("mod");
-	if (!data.functions[i].opcode)
-		return (EXIT_FAILURE);
-	data.functions[i++].f = op_mod;
-
-	data.functions[i].opcode = strdup("pchar");
-	if (!data.functions[i].opcode)
-		return (EXIT_FAILURE);
-	data.functions[i++].f = pchar;
-
-	data.functions[i].opcode = strdup("pstr");
-	if (!data.functions[i].opcode)
-		return (EXIT_FAILURE);
-	data.functions[i++].f = pstr;
-
-	data.functions[i].opcode = strdup("rotl");
-	if (!data.functions[i].opcode)
-		return (EXIT_FAILURE);
-	data.functions[i++].f = rotl;
-
-	data.functions[i].opcode = strdup("rotr");
-	if (!data.functions[i].opcode)
-		return (EXIT_FAILURE);
-	data.functions[i++].f = rotr;
-
-	data.functions[i].opcode = strdup("stack");
-	if (!data.functions[i].opcode)
-		return (EXIT_FAILURE);
-	data.functions[i++].f = change_into_stack;
-
-	data.functions[i].opcode = strdup("queue");
-	if (!data.functions[i].opcode)
-		return (EXIT_FAILURE);
-	data.functions[i++].f = change_into_queue;
-	return (0);
+	return (initialize_data2(i));
 }
