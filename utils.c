6bf5f14d5c85ce7c08f2d2f5ce3b2418e27f10e9
Nicolas Boute <4249@holbertonschool.com>
2022-05-11 16:32:38 +0200

    typo in function doc

--- utils.c
+++ utils.c
@@ -26,7 +26,7 @@ int		malloc_perror(int ret)
 
 /**
  * is_number - Checks if string is a number
- * @str: String
+ * @s: String
  * Return: 1 if it's a number, 0 if not
  */
 int		is_number(char *s)
